b0280aea9d88e76e9250d9f8e1b95afb49ad78a7
Santiago Botero <98826652+boterop@users.noreply.github.com>
2026-08-31 09:55:09 -0500

    Setup (#1)
    * add: Readme file created
    
    * Fix: CI updated to match spring boot project
    
    * Docker setup created
    
    * Setup code styles
    
    * Create /health endpoint to check API first response

--- src/test/kotlin/com/project/advanced/project/ProjectApplicationTests.kt
+++ src/test/kotlin/com/project/advanced/project/ProjectApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ProjectApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
